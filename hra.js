9489facb07d78b24efd0747d22392bf324d4fcda
Tereza Zahradníková <tereza.zahradnikova@centrum.cz>
2023-04-20 20:21:04 +0200

    part of the code moved inside the handleButtonClick function

--- hra.js
+++ hra.js
@@ -19,23 +19,25 @@ const handleButtonClick = (e) => {
     activePlayer.classList.toggle("cross");
     }
 
+    const fieldSign = fieldArray.map((sign) => {
+        if (sign.classList.contains('btn--cross')) {
+            return 'x';
+        } else if (sign.classList.contains('btn--circle')) {
+            return 'o';
+        } else {
+            return '_';
+        }
+    });
+    
+    const winner = findWinner(fieldSign);
+    
+    if (winner === "o" || winner === "x") {alert(`Vyhrál hráč se symbolem ${winner}.`)}; 
+
 };
 
 const buttons = document.querySelectorAll('.button'); //selects all buttons
 const fieldArray = Array.from(buttons); //creates and array from the selected buttons
 
-const fieldSign = fieldArray.map((sign) => {
-    if (sign.classList.contains('btn--cross')) {
-        return 'x';
-    } else if (sign.classList.contains('btn--circle')) {
-        return 'o';
-    } else {
-        return '_';
-    }
-});
 
-const winner = findWinner(fieldSign);
-
-if (winner === "o" || winner === "x") {alert(`Vyhrál hráč se symbolem ${winner}.`)}; 
 
 buttons.forEach((button) => button.addEventListener("click", handleButtonClick))
