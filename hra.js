7241360b08a46b2982595bc022018b21d9bf20f9
Tereza Zahradníková <tereza.zahradnikova@centrum.cz>
2023-04-20 20:07:44 +0200

    map method + winner feature added

--- hra.js
+++ hra.js
@@ -1,26 +1,41 @@
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4'
+
 let currentPlayer = "circle";
 let activePlayer = document.querySelector(".stats__active")
 
-
 const handleButtonClick = (e) => {
+
     if(currentPlayer === "circle") 
-    {e.target.classList.add("btn--circle"); 
+    {e.target.classList.add("btn--circle");
     currentPlayer = "cross";
     e.target.disabled = true;
     activePlayer.removeAttribute("background-color");
     activePlayer.classList.toggle("cross");
-    }
+    }  
     else
     {e.target.classList.add("btn--cross");
     currentPlayer = "circle";
     e.target.disabled = true;
     activePlayer.classList.toggle("cross");
-    };
+    }
+
 };
 
-const buttons = document.querySelectorAll(".button")
+const buttons = document.querySelectorAll('.button'); //selects all buttons
+const fieldArray = Array.from(buttons); //creates and array from the selected buttons
+
+const fieldSign = fieldArray.map((sign) => {
+    if (sign.classList.contains('btn--cross')) {
+        return 'x';
+    } else if (sign.classList.contains('btn--circle')) {
+        return 'o';
+    } else {
+        return '_';
+    }
+});
+
+const winner = findWinner(fieldSign);
+
+if (winner === "o" || winner === "x") {alert(`Vyhrál hráč se symbolem ${winner}.`)}; 
 
 buttons.forEach((button) => button.addEventListener("click", handleButtonClick))
-
-
-
